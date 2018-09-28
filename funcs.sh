370eb5d8af033bae8e900f887a74eb314582f8b3
Tyler W Hanavan <tylerhanavan@gmail.com>
2018-09-28 13:45:33 -0400

    added global variables to funcs.sh

--- funcs.sh
+++ funcs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+context=0
+quote=0
+depth=0
+
 function p {
   for _ in "$@"
     do
       echo "$_"
     done
+}
+
+function switch_context {
+  context = $1
 }
 
 function get_context {
